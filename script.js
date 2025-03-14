277890da1129ea438af8ae07e91fa14de5ffac74
Venkateswarlu Avulamanda <venkatesh78199@gmail.com>
2025-03-14 12:21:25 +0530

    Initial Design

---
+++ script.js
@@ -0,0 +1,15 @@
+
+function changeTextColor(color) {
+    var text = document.getElementById('text');
+    text.style.color = color;
+}
+
+function changeBackgroundColor(color) {
+    var background = document.getElementById('text');
+    background.style.backgroundColor = color;
+}
+
+function changeFont(font) {
+    var text = document.getElementById('text');
+    text.style.fontFamily = font;
+}
